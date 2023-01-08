d17a2e3db2d0051099cbdadc867adbfc80de5d30
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-08 15:06:07 +0300

    Сделала диаграмму и псевдокод 2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая
+// принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+if(max > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if(max > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine(max);
