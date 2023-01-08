91b57a755df692c936d19cac064a5566491e1aa8
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-08 13:52:43 +0300

    Сделала диаграмму и псевдокод

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на
+//вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+int a = 5;
+int b = 7;
+
+int max = 0;
+int min = 0;
+
+if(a < b)
+{
+    max = b;
+    min = a;
+}
+else 
+{
+    max = a;
+    min = b;
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
